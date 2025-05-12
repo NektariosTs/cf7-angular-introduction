627b03cfb435997ca6b45ad86e66fdcb210aa87b
nektarios.tsagkaris <nektarios.tsagkaris@gmail.com>
2025-05-12 17:53:18 +0300

    Person/Eperson template

--- src/app/components/person-table/person-table.component.ts
+++ src/app/components/person-table/person-table.component.ts
@@ -1,21 +1,39 @@
 import { Component, Input } from '@angular/core';
+import { EPerson } from 'src/app/shared/interfaces/eperson';
 import { Person } from 'src/app/shared/interfaces/person';
 
 @Component({
   selector: 'app-person-table',
   imports: [],
   templateUrl: './person-table.component.html',
-  styleUrl: './person-table.component.css'
+  styleUrl: './person-table.component.css',
 })
 export class PersonTableComponent {
-  @Input() personInput: Person | undefined;
-  
-    name = "Thanasis"
+  @Input() personInput: Person | EPerson | undefined;
 
-    person = {
-      givenName: " Thanasis",
-      surName: "Androutsos",
-      age: 20,
-      email: "thanasis@aueb.gr"
+  name = 'Thanasis';
+  addressOrEducation: string = ' ';
+
+  person = {
+    givenName: ' Thanasis',
+    surName: 'Androutsos',
+    age: 20,
+    email: 'thanasis@aueb.gr',
+  };
+
+  isPerson(): boolean {
+    if (this.personInput && 'address' in this.personInput) {
+      this.addressOrEducation = this.personInput.address;
+      return 'address' in this.personInput;
     }
+    return false;
+  }
+
+  isEperson(): boolean {
+    if (this.personInput && 'education' in this.personInput) {
+      this.addressOrEducation = this.personInput.education;
+      return 'education' in this.personInput;
+    }
+    return false;
+  }
 }
