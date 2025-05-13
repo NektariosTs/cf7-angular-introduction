78a120e11dfdae8ee5afc856166dde0363a9caef
nektarios.tsagkaris <nektarios.tsagkaris@gmail.com>
2025-05-13 14:14:58 +0300

    ReactiveForms

--- src/app/components/list-group-menu/list-group-menu.component.ts
+++ src/app/components/list-group-menu/list-group-menu.component.ts
@@ -5,15 +5,17 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   selector: 'app-list-group-menu',
   imports: [RouterLink, RouterLinkActive],
   templateUrl: './list-group-menu.component.html',
-  styleUrl: './list-group-menu.component.css'
+  styleUrl: './list-group-menu.component.css',
 })
 export class ListGroupMenuComponent {
   menu = [
-    { text: "Component Input Example", linkName: "component-input-example" },
-    { text: 'Component Output Example', linkName: 'component-output-example'},
-    { text: "@for Directive Example", linkName: "for-directive-example" },
-    { text: "Event-Bind-Example", linkName: "event-bind-example" },
-    { text: "Simple DataTable Example", linkName: "simple-datatable-example"},
-    { text: "Template Driven Form Example", linkName: "template-driven-form-example"}
-  ]
+    { text: 'Component Input Example', linkName: 'component-input-example' },
+    { text: 'Component Output Example', linkName: 'component-output-example' },
+    { text: '@for Directive Example', linkName: 'for-directive-example' },
+    { text: 'Event-Bind-Example', linkName: 'event-bind-example' },
+    { text: 'Simple DataTable Example', linkName: 'simple-datatable-example' },
+    {
+      text: 'Template Driven Form Example', linkName: 'template-driven-form-example' },
+    { text: 'Reactive Form Example', linkName: 'reactive-form-example' },
+  ];
 }
